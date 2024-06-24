7e901bd449a46ffdd9457caaeacc1e8f51b61964
police-amunice <sundov97@gmail.com>
2024-06-24 21:00:58 +1000

    game-logic: landed timer
    Had to disable SSR for the page. Anyway, didn't use it.
    
    TODO – start count only after onboarding.

--- src/lib/components/header/Timer.svelte
+++ src/lib/components/header/Timer.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
-	// your script goes here
+	import moment from 'moment';
+	import { onDestroy, onMount } from 'svelte';
+
+	let counter = 0;
+	let duration: moment.Duration;
+	let time = '00:00:00';
+
+	// TODO start count only after onboarding
+	function increment(): void {
+		if (document.hidden) return;
+		duration = moment.duration(++counter * 1000);
+		time = getTime(duration);
+	}
+
+	function getTime(duration: moment.Duration): string {
+		return `${getDigit(duration.hours())}:${getDigit(duration.minutes())}:${getDigit(
+			duration.seconds()
+		)}`;
+	}
+
+	function getDigit(number: number): string {
+		return number.toString().padStart(2, '0');
+	}
+
+	let clear: number;
+	$: {
+		clear = setInterval(increment, 1000);
+	}
+
+	onMount(increment);
+	onDestroy(() => clearInterval(clear));
 </script>
 
-<span>01:05:52</span>
-
-<style>
-	/* your styles go here */
-</style>
+<span class="font-mono text-sm text-muted-foreground">{time}</span>
