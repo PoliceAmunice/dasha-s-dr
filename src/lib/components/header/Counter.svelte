3f364c90829819f25aba684be355b01d82487e1d
police-amunice <sundov97@gmail.com>
2024-06-27 02:14:39 +1000

    todoes: wrapped wrong countings with humour
    Instead of fixing it, yes.

--- src/lib/components/header/Counter.svelte
+++ src/lib/components/header/Counter.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
 	import { matches, CARDS_MAP, UPPER_CARDS } from '$lib/store';
-	import { onMount } from 'svelte';
+	import clsx from 'clsx';
 
 	const cardsToOpen = (CARDS_MAP.size - UPPER_CARDS.length) / 2 - 2;
 	let counter = `0/${cardsToOpen}`;
+	let isStonks = false;
 
-	onMount(() => matches.subscribe((m) => (counter = `${m.length}/${cardsToOpen}`)));
+	matches.subscribe((m) => {
+		counter = `${m.length}/${cardsToOpen}`;
+		isStonks = m.length > cardsToOpen;
+	});
 </script>
 
-<span>Открыто экземпляров: <span class="font-mono">{counter}</span> 📈</span>
+<span
+	>Открыто экземпляров: <span class={clsx('font-mono', isStonks && 'stonks')}>{counter}</span> 📈</span
+>
+
+<style>
+	span {
+		position: relative;
+	}
+
+	span.stonks::after {
+		content: '🗿';
+		position: absolute;
+		right: -38px;
+		bottom: 2px;
+		font-size: 2rem;
+	}
+</style>
